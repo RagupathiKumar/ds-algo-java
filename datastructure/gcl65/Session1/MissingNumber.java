e8c7ba50d1f8dcd26f4aa026a6d7edeadc050bac
Ragu <kragu1894@gmail.com>
2023-12-06 22:56:06 +0530

    GCL65 :: Session1 :: MissingNumber

--- datastructure/gcl65/Session1/MissingNumber.java
+++ datastructure/gcl65/Session1/MissingNumber.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class MissingNumber {
 
     public static void main(String[] args) {
-        int[] arr = {3, 1, 0, 4};
-        int size = 4;
+        int[] arr = {3, 1, 0, 4, 2};
+        int size = arr.length;
 
         // this is to find the SUM OF N NATURAL NUMBERS -- (N * (N + 1))/2
-        int actualSum = (size * (size + 1)) / 2;
+        int actualSum = size * (size + 1) / 2;
         int arraySum = Arrays.stream(arr).sum();
 
         System.out.println("Missing number is " + (actualSum - arraySum));
