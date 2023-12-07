cf9022ee37c69f89f427304864abd57e78b86451
Ragu <kragu1894@gmail.com>
2023-12-07 09:35:45 +0530

    GCL65 :: Session1 :: MoveZerosToEnd

---
+++ datastructure/gcl65/Session1/MoveZerosToEnd.java
@@ -0,0 +1,48 @@
+package gcl65.Session1;
+
+import gcl65.Common.Util;
+
+public class MoveZerosToEnd {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 0, 3, 2, 0};
+        moveZeros2(arr);
+        Util.print(arr);
+
+        arr = new int[]{0, 0, 3, 1, 0};
+        moveZeros2(arr);
+        Util.print(arr);
+    }
+
+    // BIG O of N square
+    private static void moveZeros(int[] arr) {
+        boolean traverse = true;
+        while (traverse) {
+            traverse = false;
+
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] == 0 && arr[i + 1] > 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    traverse = true;
+                }
+            }
+        }
+    }
+
+    // BIG O of N
+    private static void moveZeros2(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+
+}
