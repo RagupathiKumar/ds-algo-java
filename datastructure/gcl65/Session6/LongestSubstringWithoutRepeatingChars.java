a130e4cd9abd721379a78e843d600903fce86df5
Ragu <kragu1894@gmail.com>
2024-02-03 19:01:07 +0530

    GCL65 :: Session6 :: LongestSubstringWithoutRepeatingChars

---
+++ datastructure/gcl65/Session6/LongestSubstringWithoutRepeatingChars.java
@@ -0,0 +1,31 @@
+package gcl65.Session6;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingChars {
+
+    public static void main(String[] args) {
+        System.out.println(longestSubstring("abcadcbb"));
+        System.out.println(longestSubstring("bbbbd"));
+    }
+
+    private static int longestSubstring(String s) {
+
+        char[] arr = s.toCharArray();
+        int n = arr.length, max = Integer.MIN_VALUE;
+        int i = 0, j = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (j < n) {
+            if (!set.contains(arr[j])) {
+                set.add(arr[j++]);
+            } else {
+                max = Math.max(max, set.size());
+                set.remove(arr[i++]);
+            }
+        }
+
+        return Math.max(max, set.size());
+    }
+}
