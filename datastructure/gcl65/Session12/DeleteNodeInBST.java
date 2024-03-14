8940d907ecbc9bbcf813b809807f14147239276d
Ragu <kragu1894@gmail.com>
2024-03-14 21:55:04 +0530

    GCL65 :: Session12 :: DeleteNodeInBST

--- datastructure/gcl65/Session12/DeleteNodeInBST.java
+++ datastructure/gcl65/Session12/DeleteNodeInBST.java
@@ -1,4 +1,36 @@
 package gcl65.Session12;
 
 public class DeleteNodeInBST {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null && root.right == null) {
+                return null;
+            } else if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode leftMostNode = findLeftMostNode(root.right);
+                root.right = deleteNode(root.right, leftMostNode.val);
+                root.val = leftMostNode.val;
+            }
+        }
+        return root;
+    }
+
+    private TreeNode findLeftMostNode(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
 }
