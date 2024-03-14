a9949086bb626bf08eaf7c1d6d2a85bebda99ac2
Ragu <kragu1894@gmail.com>
2024-03-14 21:41:41 +0530

    GCL65 :: Session12 :: InsertInBST

--- datastructure/gcl65/Session12/InsertInBST.java
+++ datastructure/gcl65/Session12/InsertInBST.java
@@ -1,4 +1,20 @@
 package gcl65.Session12;
 
 public class InsertInBST {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        // base case
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        // recursive case
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+
 }
