fafd2a6f8c21643352cbb431cff48c6da8eea6ca
Ragu <kragu1894@gmail.com>
2024-05-01 11:10:22 +0530

    GCL65 :: Session13 :: KthLargestElement

--- datastructure/gcl65/Session13/KthLargestElement.java
+++ datastructure/gcl65/Session13/KthLargestElement.java
@@ -1,4 +1,21 @@
 package gcl65.Session13;
 
+import java.util.PriorityQueue;
+
 public class KthLargestElement {
+
+    public int findKthLargest(int[] nums, int k) {
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.remove();
+            }
+        }
+        
+        return minHeap.peek();
+    }
+    
 }
