11dc36463d541bb4e257ec966aa14a6ac02fd876
Ragu <kragu1894@gmail.com>
2024-05-02 07:42:29 +0530

    GCL65 :: Session13 :: MinNumberOfRefuelingStops

--- datastructure/gcl65/Session13/MinNumberOfRefuelingStops.java
+++ datastructure/gcl65/Session13/MinNumberOfRefuelingStops.java
@@ -1,4 +1,37 @@
 package gcl65.Session13;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MinNumberOfRefuelingStops {
+
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int dist = 0;
+        int noOfStn = 0;
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+
+        for (int[] station : stations) {
+            minHeap.add(station);
+        }
+
+        while (dist < target && startFuel > 0){
+
+            dist += startFuel;
+            if (dist >= target) {
+                break;
+            }
+
+            while (!minHeap.isEmpty() && dist <= minHeap.peek()[0]) {
+                maxHeap.add(minHeap.poll());
+            }
+
+            startFuel = maxHeap.poll()[1];
+            noOfStn++;
+        }
+
+        return dist >= target ? noOfStn : -1;
+    }
+
 }
