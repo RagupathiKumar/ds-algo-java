ac6f8e98b54e725b907170f82793cc6f09c0456b
Ragu <kragu1894@gmail.com>
2024-05-02 21:14:13 +0530

    GCL65 :: Session13 :: DFSofGraph

--- datastructure/gcl65/Session13/DFSofGraph.java
+++ datastructure/gcl65/Session13/DFSofGraph.java
@@ -1,4 +1,29 @@
 package gcl65.Session13;
 
+import java.util.ArrayList;
+
 public class DFSofGraph {
+
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> result = new ArrayList<>();
+        boolean[] visited = new boolean[adj.size()];
+        dfsOfGraph(adj, 0, visited, result);
+        return result;
+    }
+
+    private void dfsOfGraph(ArrayList<ArrayList<Integer>> adj, int v, boolean[] visited, ArrayList<Integer> result) {
+        // base case
+        if (visited[v]) {
+            return;
+        }
+
+        // recursive case
+        result.add(v);
+        visited[v] = true;
+
+        for (Integer neighbour : adj.get(v)) {
+            dfsOfGraph(adj, neighbour, visited, result);
+        }
+    }
+
 }
