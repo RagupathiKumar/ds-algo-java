bef284a750d5cd5ca5831b761b8bf8b89f87ee4e
Ragu <kragu1894@gmail.com>
2024-05-05 11:00:52 +0530

    GCL65 :: Session14 :: TopologicalSort

--- datastructure/gcl65/Session14/TopologicalSort.java
+++ datastructure/gcl65/Session14/TopologicalSort.java
@@ -1,4 +1,40 @@
 package gcl65.Session14;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class TopologicalSort {
+
+    //Function to return list containing vertices in Topological order.
+    public int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        boolean[] visited = new boolean[V];
+        Stack<Integer> s = new Stack<>();
+
+        // adding for loop since multiple components are possible
+        for (int v = 0; v < V; v++) {
+            topoSort(adj, v, visited, s);
+        }
+
+        int[] result = new int[V];
+        for (int i = 0; i < V; i++) {
+            result[i] = s.pop();
+        }
+        return result;
+    }
+
+    private void topoSort(ArrayList<ArrayList<Integer>> adj, int v, boolean[] visited, Stack<Integer> s) {
+        // base case
+        if (visited[v]) {
+            return;
+        }
+
+        // recursive case
+        visited[v] = true;
+        for (Integer neighbour : adj.get(v)) {
+            topoSort(adj, neighbour, visited, s);
+        }
+        s.push(v);
+    }
+
 }
