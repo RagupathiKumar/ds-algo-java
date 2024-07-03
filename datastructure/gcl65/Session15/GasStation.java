d0db10f4bf8f77cedaad161447209d8cc721e797
Ragu <kragu1894@gmail.com>
2024-07-03 07:28:17 +0530

    GCL65 :: Session15 :: GasStation

--- datastructure/gcl65/Session15/GasStation.java
+++ datastructure/gcl65/Session15/GasStation.java
@@ -1,4 +1,24 @@
 package gcl65.Session15;
 
 public class GasStation {
+
+    // I wrote a solution with O(N square) time complexity (I used to for loop)
+    // Chaman gave this solution with O(N)
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int start = 0, currBal = 0, totBal = 0;
+
+        for (int i = 0; i < n; i++) {
+            currBal += gas[i] - cost[i];
+            totBal += gas[i] - cost[i];
+
+            if (currBal < 0) { // looks like Kadane's Algo, need to confirm
+                currBal = 0;
+                start = i + 1;
+            }
+        }
+
+        return totBal >= 0 ? start : -1;
+    }
+
 }
