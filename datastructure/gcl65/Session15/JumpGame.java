6b5af051143ab44ebaccaf6a822c0a60d448a996
Ragu <kragu1894@gmail.com>
2024-07-03 07:28:37 +0530

    GCL65 :: Session15 :: JumpGame

--- datastructure/gcl65/Session15/JumpGame.java
+++ datastructure/gcl65/Session15/JumpGame.java
@@ -1,4 +1,20 @@
 package gcl65.Session15;
 
 public class JumpGame {
+
+    // solution not working for all cases
+    public boolean canJump(int[] nums) {
+        int i = nums[0] - 1, n = nums.length;
+
+        if (i > 0) {
+            while (i < n && nums[i] > 0) {
+                i += nums[i];
+            }
+        } else {
+            i = 0;
+        }
+
+        return i >= n - 1;
+    }
+
 }
