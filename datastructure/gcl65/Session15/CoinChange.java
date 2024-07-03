db52afef97a9f5c3716a3cdd631a3a35b6151f6b
Ragu <kragu1894@gmail.com>
2024-07-03 07:44:28 +0530

    GCL65 :: Session15 :: CoinChange

--- datastructure/gcl65/Session15/CoinChange.java
+++ datastructure/gcl65/Session15/CoinChange.java
@@ -1,4 +1,25 @@
 package gcl65.Session15;
 
 public class CoinChange {
+
+    public static void main(String[] args) {
+        System.out.println(new CoinChange().coinChange(new int[] {1, 2, 5, 10, 20, 50}, 37));
+        System.out.println(new CoinChange().coinChange(new int[] {1, 7, 10}, 15)); // since we used Greedy approach, this result is not right
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int cnt  = 0;
+        int i = coins.length - 1;
+
+        while (amount > 0) {
+            while (coins[i] > amount) {
+                i--;
+            }
+            amount -= coins[i];
+            cnt++;
+        }
+
+        return cnt;
+    }
+
 }
