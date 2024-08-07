852a8e28b70487e802ef871b1f458d239e049957
Ragu <kragu1894@gmail.com>
2024-08-07 11:15:15 +0530

    GCL65 :: Session16 :: LongestIncreasingSubsequenceApproach2

---
+++ datastructure/gcl65/Session16/LongestIncreasingSubsequenceApproach2.java
@@ -0,0 +1,34 @@
+package gcl65.Session16;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequenceApproach2 {
+
+    public static void main(String[] args) {
+        System.out.println(new LongestIncreasingSubsequenceApproach2().lengthOfLIS(new int[] {10,9,2,5,3,7,101,18})); // 4
+        System.out.println(new LongestIncreasingSubsequenceApproach2().lengthOfLIS(new int[] {9,2,4,10,1,6,7,15,3})); // 5
+        System.out.println(new LongestIncreasingSubsequenceApproach2().lengthOfLIS(new int[] {2,10,3,4,7})); // 4
+        System.out.println(new LongestIncreasingSubsequenceApproach2().lengthOfLIS(new int[] {8,4,6,1,9,10,2,5,7,0})); // 4
+        System.out.println(new LongestIncreasingSubsequenceApproach2().lengthOfLIS(new int[] {3,1,5,2,7})); // 3
+        System.out.println(new LongestIncreasingSubsequenceApproach2().lengthOfLIS(new int[] {7,7,7,7,7,7,7})); // 1
+    }
+
+    // Approach 2 : DP - Bottom Down Approach
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+
+}
