ee6c4dcb5a5e4db135c20c7b966c3ecaa9d1974a
Ragu <kragu1894@gmail.com>
2024-09-11 17:03:01 +0530

    GCL65 :: Session16 :: LongestCommonSubsequence

--- datastructure/gcl65/Session16/LongestCommonSubsequence.java
+++ datastructure/gcl65/Session16/LongestCommonSubsequence.java
@@ -9,6 +9,9 @@ public class LongestCommonSubsequence {
         System.out.println(lcs.longestCommonSubsequence("abcde", "xazyc")); // 2
         System.out.println(lcs.longestCommonSubsequence("abcde", "aec")); // 2
 
+        System.out.println(lcs.longestCommonSubsequence2("abcdgh", "aedfhr")); // 3
+        System.out.println(lcs.longestCommonSubsequence2("abcde", "xazyc")); // 2
+        System.out.println(lcs.longestCommonSubsequence2("abcde", "aec")); // 2
     }
 
     /**
@@ -32,6 +35,29 @@ public class LongestCommonSubsequence {
         }
 
         return dp[r - 1][c - 1];
+    }
+
+    /**
+     * replacing grid with prev and curr array
+     * space optimized
+     * */
+    public int longestCommonSubsequence2(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[] prev = new int[n + 1];
+        int[] curr = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    curr[j + 1] = 1 + prev[j]; // diagonal
+                } else {
+                    curr[j + 1] = Math.max(curr[j], prev[j + 1]); // top or left
+                }
+            }
+            prev = curr.clone();
+        }
+
+        return curr[n];
     }
 
 }
