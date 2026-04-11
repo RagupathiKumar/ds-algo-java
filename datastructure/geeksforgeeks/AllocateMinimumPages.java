617c75547464aec7d3086a2951b7d3fd111b0238
Ragu <kragu1894@gmail.com>
2026-04-11 15:02:05 +0530

    GCL65 :: session 4

---
+++ datastructure/geeksforgeeks/AllocateMinimumPages.java
@@ -0,0 +1,57 @@
+package geeksforgeeks;
+// https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+public class AllocateMinimumPages {
+
+    public static void main(String[] args) {
+        AllocateMinimumPages thisObj = new AllocateMinimumPages();
+
+        System.out.println(thisObj.findPages(new int[] {12, 34, 67, 90}, 2));
+        System.out.println(thisObj.findPages(new int[] {15, 17, 20}, 5));
+        System.out.println(thisObj.findPages(new int[] {15, 10, 19, 10, 5, 18, 7}, 5));
+        System.out.println(thisObj.findPages(new int[] {13, 31, 37, 45, 46, 54, 55, 63, 73, 84, 85}, 9));
+
+    }
+
+    public int findPages(int[] arr, int k) {
+        int n = arr.length;
+        if (n < k) {
+            return -1;
+        }
+
+        int minPages = Integer.MIN_VALUE, maxPages = 0;
+        for (int i = 0; i < n; i++) {
+            minPages = Math.max(arr[i], minPages);
+            maxPages += arr[i];
+        }
+
+        int allocatedMinPages = -1;
+        while (minPages <= maxPages) {
+            int midPages = minPages + (maxPages - minPages) / 2;
+            int studs = calculateStudents(arr, midPages, k);
+            if (studs <= k) {
+                allocatedMinPages = midPages;
+                maxPages = midPages - 1;
+            } else {
+                minPages = midPages + 1;
+            }
+        }
+        return allocatedMinPages;
+    }
+
+    private int calculateStudents(int[] arr, int minPages, int k) {
+        int n = arr.length;
+        int sumPages = 0, studs = 0;
+        for (int i = 0; i < n; i++) {
+            sumPages += arr[i];
+            if (sumPages > minPages) {
+                studs++;
+                sumPages = arr[i];
+            }
+        }
+        if (sumPages > 0) {
+            studs++;
+        }
+        return studs;
+    }
+
+}
---
+++ datastructure/geeksforgeeks/PaintersPartitionProblem.java
@@ -0,0 +1,58 @@
+package geeksforgeeks;
+
+import static java.lang.System.out;
+
+// https://www.geeksforgeeks.org/dsa/painters-partition-problem/
+public class PaintersPartitionProblem {
+
+    public static void main(String[] args) {
+        out.println(new PaintersPartitionProblem().minTime(new int[] {5, 10, 30, 20, 15}, 3)); // 35
+        out.println(new PaintersPartitionProblem().minTime(new int[] {10, 20, 30, 40}, 2)); // 60
+        out.println(new PaintersPartitionProblem().minTime(new int[] {100, 200, 300, 400}, 1)); // 1000
+    }
+
+    public int minTime(int[] arr, int k) {
+        int n = arr.length;
+        if (n < k) {
+            return -1;
+        }
+
+        int minTime = Integer.MIN_VALUE, maxTime = 0;
+        for (int i = 0; i < n; i++) {
+            minTime = Math.max(minTime, arr[i]);
+            maxTime += arr[i];
+        }
+
+        int time = -1;
+        while (minTime <= maxTime) {
+            int midTime = minTime + (maxTime - minTime) / 2;
+            int painters = calculatePainters(arr, midTime, k);
+            if (painters <= k) {
+                time = midTime;
+                maxTime = midTime - 1;
+            } else {
+                minTime = midTime + 1;
+            }
+        }
+
+        return time;
+    }
+
+    private int calculatePainters(int[] arr, int midTime, int k) {
+        int n = arr.length;
+        int time = 0, painters = 0;
+        for (int i = 0; i < n; i++) {
+            time += arr[i];
+            if (time > midTime) {
+                painters++;
+                time = arr[i];
+            }
+        }
+        if (time > 0) {
+            painters++;
+        }
+        return painters;
+    }
+
+
+}
